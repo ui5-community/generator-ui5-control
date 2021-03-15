2e9dfec138d6c6d9abb7c229e193ebac3a5813b6
Volker Buzek <volker.buzek@js-soft.com>
2021-03-15 18:40:48 +0100

    chore: restructure

---
+++ __tests__/basic.test.js
@@ -0,0 +1,4 @@
+const helpers = require("yeoman-test")
+describe("generic subgen call", () => {
+    test.todo("walk through prompt")
+})
---
+++ app/index.js
@@ -0,0 +1,41 @@
+const Generator = require("yeoman-generator")
+module.exports = class extends Generator {
+    constructor(args, opts) {
+        super(args, opts)
+
+        this.argument("namespace", { type: String, required: false })
+        this.argument("builddir", { type: String, required: false })
+    }
+
+    _writing() {
+        const namespace = this.options.namespace || this.answers.namespace
+        const builddir = this.options.builddir || this.answers.builddir
+
+        this.fs.copyTpl(
+            this.templatePath("wdio-wdi5.conf.js"),
+            this.destinationPath(this.answers.wdi5ConfPath, "wdio-wdi5.conf.js"),
+            this.answers
+        )
+    }
+
+    async prompting() {
+        const prompts = []
+        prompts.push({
+            type: "input",
+            name: "namespace",
+            message: "What's the name space your custom control(s) should live in?",
+            default: "my.ui5.cc",
+            store: true
+        })
+        prompts.push({
+            type: "input",
+            name: "builddir",
+            message: "In what directory should the custom control be stored?",
+            default: "./my/ui5/cc",
+            store: true
+        })
+        this.answers = await this.prompt(prompts)
+    }
+
+    async writing() {}
+}
